37f4323497a4845ae3ae47ae919805f85cc77109
Daniel Oropeza <fnrlnw@gmail.com>
2016-09-08 16:53:19 -0400

    Wire up main page for upvoting posts

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -15,8 +15,8 @@ angular.module('flapperNews')
 			$scope.link = '';
 		};
 
-		$scope.incrementUpvotes = function(post){
-			post.upvotes +=1 ;
+		$scope.incrementUpvotes = function(post) {
+			posts.upvote(post);
 		};
 
 	}]);
