8fb900edb488decf4886dbef0297a696dc506d0a
Daniel Oropeza <fnrlnw@gmail.com>
2016-09-08 14:25:51 -0400

    Fix responders problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  # respond_to :json ( Might not be needed thanks to the responders gem )
+require "application_responder"
 
+class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :json
+
+  protect_from_forgery with: :exception
 
   def angular
   	render 'layouts/application' 	
-  	
   end
-
 end
