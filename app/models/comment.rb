f74a77b1093b394e6419e0f510d893acbd0f9869
Daniel Oropeza <fnrlnw@gmail.com>
2016-09-09 12:53:19 -0400

    Fix model json

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-=begin
+
    def as_json(options = {})
     super(options.merge(include: :user))
    end
-=end
+
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
-  def as_json(options = {})
-    super(options.merge(include: :comments))
-  end
-=begin
 	
     def as_json(options = {})
       super(options.merge(include: [:user, comments: {include: :user}]))
-    end
-=end	
+    end	
 end
